9bb0d364e93b136be27ece39d420a57babc975be
gottagofaster236 <lr.soft.now@gmail.com>
2020-06-05 20:09:31 +0300

    Исправлено форматирование файла

--- tutorial.py
+++ tutorial.py
@@ -1,15 +1,15 @@
-#Привет, это туториал по user
-from user import * #импорт с файла user.py
+# Привет, это туториал по user
+from user import *  # импорт с файла user.py
 
-users = get_users() #получаем список юзеров
-example_user = users[0] #выбираем юзера
+users = get_users()  # получаем список юзеров
+example_user = users[0]  # выбираем юзера
 
 print("Name, id:", example_user.name, example_user.id)
 print("Printing friends")
 for friend in example_user.friends:
     print("friend:", friend.name)
 
-#обьект класса user имеет:
+# обьект класса user имеет:
 #   Поле name
 #   Поле id
 #   Поле friends (список)
