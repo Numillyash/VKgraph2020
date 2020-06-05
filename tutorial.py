5fceafcf1b5ae55c1c6c11fdfd79a30dae38dee8
Numillyash <you@example.com>
2020-06-05 19:58:32 +0300

    Добавлен туториал

---
+++ tutorial.py
@@ -0,0 +1,15 @@
+#Привет, это туториал по user
+from user import * #импорт с файла user.py
+
+users = get_users() #получаем список юзеров
+example_user = users[0] #назначаем юзера
+
+print("Name, id:", example_user.name, example_user.id)
+print("Printing friends")
+for friend in example_user.friends:
+    print("friend:", friend.name)
+
+#обьект класса user имеет:
+#   Поле name
+#   Поле id
+#   Поле friends (список)
