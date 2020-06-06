8a12b5952f42392a3d470d5b045c38aa10f8bdde
gottagofaster236 <lr.soft.now@gmail.com>
2020-06-06 15:13:46 +0300

    Получаю людей по именам, а не по id

--- Task 3.py
+++ Task 3.py
@@ -6,8 +6,8 @@ g = get_graph()
 clustering = louvain.find_partition(g, louvain.CPMVertexPartition)
 print(len(clustering), "clusters")
 
-#target_index = get_index_of_user(get_user_by_name("Vsevolod Lavrov"))
-target_index = get_index_of_user(get_user_by_name("Daniil Grinchenko"))
+#target_index = get_index_of_user(get_user_by_name("Vsevolod Lavrov"))  # 11-1 класс и матцентр 11 класса
+target_index = get_index_of_user(get_user_by_name("Daniil Grinchenko"))  # параллель 10-х классов
 
 for cluster in clustering:
     if target_index in cluster:
