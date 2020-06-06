427765ec07532d85fd32b92e93f5326a3097fb7e
gottagofaster236 <lr.soft.now@gmail.com>
2020-06-06 16:39:14 +0300

    Добавлен ввод из консоли

--- Task 3.py
+++ Task 3.py
@@ -3,16 +3,22 @@ from igraph_util import *
 
 users = get_users()
 clusters = get_clusters()
-print("length of clusters:", len(clusters))
+print("Количество кластеров:", len(clusters))
 
-#target_user = get_user_by_name("Vsevolod Lavrov")  # 11-1 класс и матцентр 11 класса
-#target_user = get_user_by_name("Daniil Grinchenko")  # параллель 10-х классов
+answer = input("Введите id пользователя, либо имя и фамилию латиницей: ")
+if answer.isdigit():
+    target_user = get_user_by_id(int(answer))
+else:
+    target_user = get_user_by_name(name)
+
+#target_user = get_user_by_name("Georgy Ulanovsky")  # 10-4 класс
 #target_user = get_user_by_name("Roman Mikhaylov")  # часть 10-1 класса
+#target_user = get_user_by_name("Daniil Grinchenko")  # ещё параллель 10-х классов
+#target_user = get_user_by_name("Vsevolod Lavrov")  # 11-1 класс и матцентр 11 класса
 #target_user = get_user_by_name("Lev Leontyev")  # oops
-target_user = get_user_by_name("Georgy Ulanovsky")
 
 for cluster in clusters:
     if target_user in cluster:
-        print("size of current cluster:", len(cluster))
+        print("Размер текущего кластера:", len(cluster))
         for user in cluster:
             print(user.name)
