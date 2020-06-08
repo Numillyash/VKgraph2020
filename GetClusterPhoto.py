4dc23bfccfa29de203d93042c5115442a0c0737f
Numillyash <you@example.com>
2020-06-08 22:36:50 +0300

    Фото кластеров (Починены фото, попытка 2)

--- GetClusterPhoto.py
+++ GetClusterPhoto.py
@@ -7,7 +7,10 @@ import pygraphviz as pgv
 def getClPhoto():
     clusters = get_clusters()
     ind = 0
+    obsch = 0
     for i in clusters:
+        obsch+=1
+        print(obsch)
         if(len(i) >= 10):
             ind += 1
             g = pgv.AGraph(mode = "mds")
@@ -15,11 +18,12 @@ def getClPhoto():
             for people in i:
                 if(not people.name in g.nodes()):
                     g.add_node(people.name)
+            print("Done nodes")
             for people in i:
                 for friend in people.friends:
                     if friend in i:
                         g.add_edge(people.name, friend.name)
-
+            print("Done edges")
             g.draw(f"Clusters/cluster{ind}.png", prog="circo", args = "")
             print(f"End number {ind}")
 
